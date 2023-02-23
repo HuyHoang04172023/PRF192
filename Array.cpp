7b891fffc61d7e1eb2fd37a50a758887aa52df4b
HuyHoang04172023 <125451388+HuyHoang04172023@users.noreply.github.com>
2023-02-24 03:53:48 +0700

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -18,19 +18,19 @@ main (){
 }
 
 
-//Vi du 1 and 3  
-//#include <stdio.h>
-//main (){
-//	int n,i;
-//	int a[1000];
-//	printf("Input size of array:");
-//	scanf("%d",&n);
-//	for(i=0;i<n;i++){
-//		printf("a[%d] =", i);
-//		scanf("%d",&a[i]);
-//	}
-//	printf("\nOUTPUT\n");
-//	for(i=0;i<n;i++){
-//		printf("%d: ",a[i]);
-//	} 
-//}
+//Vi du 1 and 2 
+#include <stdio.h>
+main(){
+	int n,i;
+	int a[1000];
+	printf("Size of array: ");
+	scanf("%d", &n);
+	for (i=0;i<n;i++){
+		printf("a[%d] =", i);
+		scanf("%d", &a[i]);
+	}
+	for(i=0;i<n;i++) {
+		if (i==n-1) printf("%d ,a[i]);
+		else printf("%d : ",a[i]);
+	}
+}

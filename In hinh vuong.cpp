f6d8de411838c50345bf4d569fbfa6884dd68d12
HuyHoang04172023 <125451388+HuyHoang04172023@users.noreply.github.com>
2023-02-26 04:58:01 +0700

    Update In hinh vuong.cpp

--- In hinh vuong.cpp
+++ In hinh vuong.cpp
@@ -1,42 +1,42 @@
 
 //Hay in ra hinh vuong dac co kich thuoc n bang ky tu "*".
-//#include <stdio.h>
-//int main()
-//{
-//    int n;
-//    printf("Nhap do dai canh: ");
-//    scanf("%d", &n);
-//    int i, j;
-//    for(i = 0; i<n; i++){
-//        for(j = 0; j<n; j++){
-//            printf("* ");
-//        }
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main()
+{
+   int n;
+   printf("Nhap do dai canh: ");
+   scanf("%d", &n);
+   int i, j;
+   for(i = 0; i<n; i++){
+       for(j = 0; j<n; j++){
+           printf("* ");
+       }
+       printf("\n");
+   }
+}
 
 
 //Hay in ra hinh vuong rong co kich thuoc n xn, bang ky tu "*".
-//#include <stdio.h>
-//int main()
-//{
-//    int n;
-//    printf("Nhap do dai canh: ");
-//    scanf("%d", &n);
-//    int i, j;
-//    for(i = 0; i<n; i++){
-//        for(j = 0; j<n; j++){
-//         //Neu nam tren duong vien thi ta in ra ky tu ""
-//            if(i == 0 || i == n-1 || j == 0 || j == n-1){
-//                printf("* ");
-//            }//Nguoc lai ta in ra ky tu khoang trang 
-//           else{
-//                printf("  ");
-//            }
-//        }
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main()
+{
+   int n;
+   printf("Nhap do dai canh: ");
+   scanf("%d", &n);
+   int i, j;
+   for(i = 0; i<n; i++){
+       for(j = 0; j<n; j++){
+        //Neu nam tren duong vien thi ta in ra ky tu ""
+           if(i == 0 || i == n-1 || j == 0 || j == n-1){
+               printf("* ");
+           }//Nguoc lai ta in ra ky tu khoang trang 
+          else{
+               printf("  ");
+           }
+       }
+       printf("\n");
+   }
+}
 
 
 //In ra hinh tam giac vuong can dac o goc phan tu so I, II, III, IV.

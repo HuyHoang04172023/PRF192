a9ea45e835be9dd999883e2930035038a9f3a05d
HuyHoang04172023 <125451388+HuyHoang04172023@users.noreply.github.com>
2023-03-08 04:29:09 +0700

    Update ThemPhanTu.c

--- ThemPhanTu.c
+++ ThemPhanTu.c
@@ -1,4 +1,4 @@
-//Them phan tu vao vi tri xac dinh
+//Them phan tu vao vi tri xac dinh trong mot mang xac dinh
 #include <stdio.h>
 main (){
 	int a[1000];
@@ -25,7 +25,31 @@ main (){
 
 
 
-
+//Them phan tu vao vi tri xac dinh trong mot mang bat ky
+#include <stdio.h>
+main (){
+	int a[1000];
+	int id,n=0,i,value,index;
+	printf("Input student ID: ");
+	scanf("%d",&id);
+	for(i=id;i!=0;i=i/10) n++;
+	for(i=n-1;i>=0;i--){
+		a[i]=id%10;
+		id=id/10;
+	}
+	printf("Array: ");
+	for(i=0;i<n;i++) printf("%d ",a[i]);
+	printf("\n");
+	printf("Input value: ");
+	scanf("%d",&value);
+	printf("Input index: ");
+	scanf("%d",&index);
+	for(i=n;i>=index;i--) a[i]=a[i-1];
+	a[index-1] = value;
+	n++;
+	printf("New array: ");
+	for(i=0;i<n;i++) printf("%d ",a[i]);
+}
 
 
 

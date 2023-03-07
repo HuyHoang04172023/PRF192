d714cadc9d0e162b5005adf49c610efc3ebccc86
HuyHoang04172023 <125451388+HuyHoang04172023@users.noreply.github.com>
2023-03-08 03:10:24 +0700

    Add files via upload

---
+++ ThemPhanTu.c
@@ -0,0 +1,46 @@
+//Them phan tu vao vi tri xac dinh
+#include <stdio.h>
+main (){
+	int a[1000];
+	int n,i,value,index;
+	printf("Input size of array: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("a[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	printf("Array: ");
+	for(i=0;i<n;i++) printf("%d ",a[i]);
+	printf("\n");
+	printf("Input value: ");
+	scanf("%d",&value);
+	printf("Input index: ");
+	scanf("%d",&index);
+	for(i=n;i>=index;i--) a[i]=a[i-1];
+	a[index-1] = value;
+	n++;
+	printf("New array: ");
+	for(i=0;i<n;i++) printf("%d ",a[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
